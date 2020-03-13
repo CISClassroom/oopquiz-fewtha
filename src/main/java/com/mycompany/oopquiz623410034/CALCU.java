fec71f4e5c408fbc62412edf9af9cb84db6c47eb
fewtha <59079336+fewtha@users.noreply.github.com>
2020-03-13 16:55:04 +0700

    623410034-7

---
+++ src/main/java/com/mycompany/oopquiz623410034/CALCU.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.oopquiz623410034;
+
+/**
+ *
+ * @author Fuse TP
+ */
+public class CALCU {
+    double plus (double n1 ,double n2,double n3 ,double n4,double n5){
+        return (n1 + n2 + n3 + n4 + n5) / 2;
+    }
+    
+}
